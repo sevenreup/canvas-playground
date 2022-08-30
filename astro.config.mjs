9558d9836b24457a4eacd44c51c8af65a81aa135
Christopher Seven Phiri <chris7phiri@gmail.com>
2022-08-31 00:22:29 +0200

    add tailwind add main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sevenreup.github.io",
-  base: "/canvas",
+  base: "/canvas-playground",
+  integrations: [tailwind()]
 });
